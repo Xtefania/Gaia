42a0b765aacfaffa58ce8483fa2460465ec75d07
Xtefania <elesesnus1@gmail.com>
2025-04-23 00:13:31 -0500

    activity splash, activity main y activity inicio sesión

---
+++ app/src/main/java/com/example/gaia/Activities/MainHActivity.kt
@@ -0,0 +1,17 @@
+package com.example.gaia.Activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.gaia.R
+
+class MainHActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
