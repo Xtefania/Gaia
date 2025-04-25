38b75ef135e915ae56b884a85b718a9d6f40474a
Ana-Maria-Ruiz-Salcedo <anamariaruizsalcedo@gmail.com>
2025-04-25 15:52:43 -0500

    pull

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.gaia"
-        minSdk = 33
+        minSdk = 21
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
