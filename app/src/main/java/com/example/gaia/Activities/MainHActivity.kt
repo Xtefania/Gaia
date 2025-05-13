709c940e95cfca7687ab0bcf86eaed484265a09d
NicoleBenavides <nicolebenavides55@gmail.com>
2025-05-13 01:05:23 -0500

    Eliminación de código - DB prueba
    Se elimino el código que se utilizo en el commit " DB prueba", ya que se va a utilizar SharedPreferences.

--- app/src/main/java/com/example/gaia/Activities/MainHActivity.kt
+++ app/src/main/java/com/example/gaia/Activities/MainHActivity.kt
@@ -1,14 +1,11 @@
 package com.example.gaia.Activities
-import com.example.gaia.db.DbHelper
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.gaia.R
-
 
 class MainHActivity : AppCompatActivity() {
     // Renderización
@@ -19,21 +16,11 @@ class MainHActivity : AppCompatActivity() {
 
         // Navegación
 
-        //  - Vista crear DB
+        //  - Vista iniciar sesión
         val btnLogin1 = findViewById<Button>(R.id.btn_inicio_sesion1)
         btnLogin1.setOnClickListener {
-            val dbHelper = DbHelper(this@MainHActivity)
-            val db = dbHelper.writableDatabase
-
-            if (db != null) {
-                Toast.makeText(this, "Base de datos creada correctamente", Toast.LENGTH_SHORT).show();
-
-                //  - Vista iniciar sesión
-                val intent = Intent(this, LoginActivity::class.java)
-                 startActivity(intent)
-            } else {
-                Toast.makeText(this, "Error al crear la base de datos", Toast.LENGTH_SHORT).show();
-            }
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
         }
 
         // - Vista registrar
